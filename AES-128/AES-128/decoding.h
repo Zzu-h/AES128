7828c1ad6a713711259b5b36ef16e9d710a16536
Zzu-h <juho7785@naver.com>
2021-05-17 14:23:57 +0900

    Update

--- AES-128/AES-128/decoding.h
+++ AES-128/AES-128/decoding.h
@@ -14,14 +14,19 @@ private:
 	const char* cipher_Path;
 	const char* plain_Path;
 
+	Key curKey[KeySize];
 	char ciphertext[CipherSize] = { 0 };
 	char plaintext[PlainSize] = { 0 };
 
-	errno_t binCopy();
+	bool ver = polynomial_standard;
+
+	void Copy();
 	void Substitute();
 	void ShiftRows();
 	errno_t MixColumns();
+	char Scalar(int, char*);
 	void AddRoundKey();
+	void getCurKey(int);
 public:
 	decoding(const char*, const char*, const char*);
 	errno_t doDecoding();
